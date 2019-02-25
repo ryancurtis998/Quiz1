621284fa8a6cc24ffe5cb86ee6d999bd7fe2494b
santa <ryancurtis998@yahoo.com>
2019-02-25 14:24:36 +0300

    addition of progress bar to the js

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,14 @@
 
       });
 });
+// scrolldown progress bar
+$(window).scroll(function() {
+  var s = $(window).scrollTop(),
+    d = $(document).height(),
+    c = $(window).height();
+  scrollPercent = (s / (d - c)) * 100;
+  var position = scrollPercent;
+
+  $("#progressbar").attr('value', position);
+
+});
